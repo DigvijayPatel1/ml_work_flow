0d109f468f7694582994ac92a5af3be3238d2dc4
Digvijay Patel <dpat19122@gmail.com>
2026-04-03 11:27:29 +0530

    added usvisa data from the mongo db to data frame

---
+++ us_visa/data_access/usvisa_data.py
@@ -0,0 +1,38 @@
+from us_visa.configuration.mongo_db_connection import MongoDBClient
+from us_visa.constants import DATABASE_NAME
+from us_visa.exception import USVisaException
+import pandas as pd
+import sys
+from typing import Optional
+import numpy as np
+
+class USVisaData:
+    """
+    this help to import entire mongo db collection as dataframe
+    """
+    def __init__(self):
+        try:
+            self.mongo_db_client = MongoDBClient(database_name=DATABASE_NAME)
+        except Exception as e:
+            raise USVisaException(e, sys)
+    
+    def export_collection_as_dataframe(self, collection_name: str, database_name: Optional[str] = None) -> pd.DataFrame:
+        """
+        this method exports the dataframe from mongodb collection as dataframe 
+        
+        Output      :   dataframe
+        On Failure  :   raises an exception
+        """
+        try:
+            if database_name is None:
+                database_name = self.mongo_db_client.database_name
+            else:
+                collection = self.mongo_db_client.client[database_name][collection_name]
+            
+            data = list(collection.find())
+            df = pd.DataFrame(data)
+            if "_id" in df.columns:
+                df.drop(columns=["_id"], inplace=True)
+            return df
+        except Exception as e:
+            raise USVisaException(e, sys)
