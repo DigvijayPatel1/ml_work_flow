b9f2c3db3505728998fde8d1bc62f8d7a01c525d
Digvijay Patel <dpat19122@gmail.com>
2026-04-08 01:06:54 +0530

    updated the artifact

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -15,6 +15,6 @@ class DataValidationArtifact:
 class DataTransformationArtifact:
     transformed_train_file_path: str
     transformed_test_file_path: str
-    preprocessed_object_file_path: str
+    transformed_object_file_path: str
 
     
